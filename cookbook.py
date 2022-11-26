93ccb38856889ab040047749d7d9b4f1d2df8ef5
Kotovski <roman_kotovski@mail.ru>
2022-11-26 18:38:02 +0500

    Third task

--- cookbook.py
+++ cookbook.py
@@ -1,4 +1,6 @@
+import os
 
+#задание 1
 def reading_book() -> dict:
     with open ('recipes.txt', 'r', encoding='utf8') as file:
         dict = {}
@@ -12,7 +14,7 @@ def reading_book() -> dict:
     return dict
 
 
-
+#задание 2
 def list_of_stores_with_ingredients(dishes: list, person_count: int, cook_book: dict) -> dict:
     result = {}
     for dish in dishes:
@@ -27,5 +29,17 @@ def list_of_stores_with_ingredients(dishes: list, person_count: int, cook_book: 
             print(f'Блюда "{dish}" нет в книге рецептов')
     return result
 
+
+#задание 3
+os.chdir(r"sorted")
+list_files = []
+for file in os.listdir():
+    with open(file, 'r', encoding='utf-8') as f:
+        text = f.read()
+        list_files.append([text.count('\n') + 1, file, text])
+list_files.sort()
+with open('output.txt', 'x', encoding='utf-8') as f:
+    for lf in list_files:
+        f.write(lf[1]+'\n'+str(lf[0])+'\n'+lf[2]+'\n')
 
 
